1fbc07a04dbdf902d03518b083b25c548a314ee0
Nercus <48932939+Nercus@users.noreply.github.com>
2025-05-25 00:45:13 +0200

    chore: clean up NercUtils repository by removing .git directory

--- install-deps.sh
+++ install-deps.sh
@@ -3,3 +3,4 @@
 svn checkout https://repos.wowace.com/wow/libstub/tags/1.0 ./libs/LibStub
 svn checkout https://repos.wowace.com/wow/callbackhandler/trunk/CallbackHandler-1.0 ./libs/CallbackHandler-1.0
 git clone https://github.com/Nercus/NercUtils.git ./libs/NercUtils
+rm -rf ./libs/NercUtils/.git
